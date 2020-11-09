62ae1a014817e51c3e44625dfc88697b107e9cf5
shashidhar0711 <shashidhar.sasic@gmail.com>
2020-11-09 23:39:20 +0530

    [sasic] Add . Creating a BInary Search Tree and Inserting values

---
+++ DataStructureBinarySearchTree/BinarySearchTree.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructureBinarySearchTree
+{
+    /// <summary>
+    /// Binary search tree generic class 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class BinarySearchTree<T> where T:IComparable
+    {
+        /// <summary>
+        /// Gets or sets the node data.
+        /// </summary>
+        /// <value>
+        /// The node data.
+        /// </value>
+        public T NodeData { get; set; }
+
+        /// <summary>
+        /// Gets or sets the left tree.
+        /// </summary>
+        /// <value>
+        /// The left tree.
+        /// </value>
+        public BinarySearchTree<T> LeftTree { get; set; }
+
+        /// <summary>
+        /// Gets or sets the right tree.
+        /// </summary>
+        /// <value>
+        /// The right tree.
+        /// </value>
+        public BinarySearchTree<T> RightTree { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BinarySearchTree{T}"/> class.
+        /// </summary>
+        /// <param name="nodeData">The node data.</param>
+        public BinarySearchTree(T nodeData)
+        {
+            this.NodeData = nodeData;
+            this.LeftTree = null;
+            this.RightTree = null;
+        }
+
+        /// <summary>
+        /// variables
+        /// </summary>
+        int leftCount = 0;
+        int rightCount = 0;
+        bool result = false;
+
+        /// <summary>
+        /// Inserts the specified item.
+        /// </summary>
+        /// <param name="item">The item.</param>
+        public void Insert(T item)
+        {
+            T currentNodeValue = this.NodeData;
+            /// It checks, if current node value is greater than given item
+            if (currentNodeValue.CompareTo(item) > 0)
+            {
+                if (this.LeftTree == null)
+                {
+                    /// create a new left tree with that item
+                    this.LeftTree = new BinarySearchTree<T>(item);
+                }
+                else
+                {
+                    /// if does have already left tree insert this new item into the left tree
+                    this.LeftTree.Insert(item);
+                }
+            }
+            else
+            {
+                if (this.RightTree == null)
+                {
+                    /// create a new right tree with that item
+                    this.RightTree = new BinarySearchTree<T>(item);
+                }
+                else
+                {
+                    /// if does have already right tree insert this new item into the right tree
+                    this.RightTree.Insert(item);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Displays this instance.
+        /// </summary>
+        public void Display()
+        {
+            if (this.LeftTree != null)
+            {
+                this.leftCount++;
+                this.LeftTree.Display();
+            }
+            Console.WriteLine(this.NodeData.ToString());
+
+            if (this.RightTree != null)
+            {
+                this.rightCount++;
+                this.RightTree.Display();
+            }
+        }
+    }
+}
--- DataStructureBinarySearchTree/Program.cs
+++ DataStructureBinarySearchTree/Program.cs
@@ -11,6 +11,14 @@ namespace DataStructureBinarySearchTree
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Binary Search Tree");
+            /// Creating a reference of object class
+            BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);
+            /// Inserting elements in to binary search tree
+            binarySearch.Insert(56);
+            binarySearch.Insert(30);
+            binarySearch.Insert(70);
+            /// printing all elements
+            binarySearch.Display();
         }
     }
 }
