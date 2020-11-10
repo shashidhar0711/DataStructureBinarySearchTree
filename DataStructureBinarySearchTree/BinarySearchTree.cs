01a31e53403627d0b14ec1d880d68e7aa649268f
shashidhar0711 <shashidhar.sasic@gmail.com>
2020-11-10 20:45:52 +0530

    [sasic] Add . Ability to Search Element in BST

---
+++ DataStructureBinarySearchTree/BinarySearchTree.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructureBinarySearchTree
+{
+    public class BinarySearchTree<T> where T:IComparable
+    {
+        /// <summary>
+        /// Gets or sets the node data.
+        /// </summary>
+        /// <value>
+        /// The node data.
+        /// </value>
+        public T NodeData { get; set; }
+
+        /// <summary>
+        /// Gets or sets the left tree.
+        /// </summary>
+        /// <value>
+        /// The left tree.
+        /// </value>
+        public BinarySearchTree<T> LeftTree { get; set; }
+
+        /// <summary>
+        /// Gets or sets the right tree.
+        /// </summary>
+        /// <value>
+        /// The right tree.
+        /// </value>
+        public BinarySearchTree<T> RightTree { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BinarySearchTree{T}"/> class.
+        /// </summary>
+        /// <param name="nodeData">The node data.</param>
+        public BinarySearchTree(T nodeData)
+        {
+            this.NodeData = nodeData;
+            this.LeftTree = null;
+            this.RightTree = null;
+        }
+
+        /// <summary>
+        /// variables
+        /// </summary>
+        
+        bool result = false;
+        int leftCount = 0;
+        int rightCount = 0;
+
+        /// <summary>
+        /// Inserts the specified item.
+        /// </summary>
+        /// <param name="item">The item.</param>
+        public void Insert(T item)
+        {
+            T currentNodeValue = this.NodeData;
+            /// It checks, if current node value is greater than given item
+            if (currentNodeValue.CompareTo(item) > 0)
+            {
+                if (this.LeftTree == null)
+                {
+                    /// create a new left tree with that item
+                    this.LeftTree = new BinarySearchTree<T>(item);
+                }
+                else
+                {
+                    /// if does have already left tree insert this new item into the left tree
+                    this.LeftTree.Insert(item);
+                }
+            }
+            else
+            {
+                if (this.RightTree == null)
+                {
+                    /// create a new right tree with that item
+                    this.RightTree = new BinarySearchTree<T>(item);
+                }
+                else
+                {
+                    /// if does have already right tree insert this new item into the right tree
+                    this.RightTree.Insert(item);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Displays this instance.
+        /// </summary>
+        public void Display()
+        {
+            if (this.LeftTree != null)
+            {
+                this.leftCount++;
+                this.LeftTree.Display();
+            }
+            Console.WriteLine(this.NodeData.ToString());
+
+            if (this.RightTree != null)
+            {
+                this.rightCount++;
+                this.RightTree.Display();
+            }
+        }
+
+        /// <summary>
+        /// Gets the size.
+        /// </summary>
+        /// <returns></returns>
+        public void GetSize()
+        {
+            Console.WriteLine("size" + " " + (1 + this.leftCount + this.rightCount));
+        }
+
+    /// <summary>
+    /// Searches the element.
+    /// </summary>
+    /// <param name="element">The element.</param>
+    /// <param name="node">The node.</param>
+    /// <returns></returns>
+    public bool SearchElement(T element, BinarySearchTree<T> node)
+        {
+            if(node == null)
+            {
+                return false;
+            }
+
+            /// If node data equals to element
+            /// it will print the node data
+            if(node.NodeData.Equals(element))
+            {
+                Console.WriteLine("Found the element " +node.NodeData);
+                return true;
+            }
+
+            /// if element is lesser than node data then it will search on left tree or nodes
+            /// if element is greater than node data then it will search on right tree or nodes
+            if(element.CompareTo(node.NodeData) < 0)
+            {
+                SearchElement(element, node.LeftTree);
+            }
+            if (element.CompareTo(node.NodeData) > 0)
+            {
+                SearchElement(element, node.RightTree);
+            }
+            return result;
+        }
+    }
+}
--- DataStructureBinarySearchTree/Program.cs
+++ DataStructureBinarySearchTree/Program.cs
@@ -11,6 +11,27 @@ namespace DataStructureBinarySearchTree
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Binary Search Tree");
+            /// Creating a reference of object class
+            BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);
+            /// Inserting elements in to binary search tree
+            binarySearch.Insert(30);
+            binarySearch.Insert(70);
+            binarySearch.Insert(22);
+            binarySearch.Insert(40);
+            binarySearch.Insert(60);
+            binarySearch.Insert(95);
+            binarySearch.Insert(11);
+            binarySearch.Insert(65);
+            binarySearch.Insert(3);
+            binarySearch.Insert(16);
+            binarySearch.Insert(63);
+            binarySearch.Insert(67);
+            /// printing all elements
+            /// it will prit size of binary tree
+            binarySearch.Display();
+
+            /// seaching elements if it exist it will print or not
+            bool result = binarySearch.SearchElement(67, binarySearch);
         }
     }
 }
